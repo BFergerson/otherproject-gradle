f86a05fef59e1e9ee575625c7c2aa8f7321a04c9
Brandon Fergerson <fergerson92@gmail.com>
2018-07-27 19:34:31 -0400

    init

---
+++ src/main/java/com/gitdetective/App2.java
@@ -0,0 +1,31 @@
+package com.gitdetective;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App2 {
+    static {
+        new MyClass().myMethod();
+        System.out.println(String.valueOf(10));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        List arrayList = Lists.newArrayList();
+        MyClass yay = new MyClass();
+        arrayList.add(yay);
+
+        yay.myMethod();
+        System.out.println(arrayList.size());
+    }
+
+    public static void anotherOne() {
+        MyClass yay = new MyClass();
+        yay.myMethod();
+    }
+}
