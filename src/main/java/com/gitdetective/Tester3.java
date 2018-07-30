df5bb33b770920bc41f8308f23e8541dce57f68a
Brandon Fergerson <fergerson92@gmail.com>
2018-07-30 13:41:06 -0400

    test

---
+++ src/main/java/com/gitdetective/Tester3.java
@@ -0,0 +1,15 @@
+package com.gitdetective;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tester3 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Tester3.class);
+
+    static {
+        logger.info("11");
+        logger.info("22");
+    }
+
+}
