264b53b9f00d98153a542da6a34ad99ee04b518f
Brandon Fergerson <fergerson92@gmail.com>
2018-08-01 16:49:59 -0400

    test

--- src/main/java/com/gitdetective/Tester2.java
+++ src/main/java/com/gitdetective/Tester2.java
@@ -1,6 +1,25 @@
 package com.gitdetective;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+
 public class Tester2 {
+    static Logger LOGGER = LoggerFactory.getLogger(Tester2.class);
+
+    static {
+        Iterator itr = null;
+        while (itr.hasNext()) {
+            Object ext = null;
+            if (ext == null) {
+                LOGGER.error("failed to load extension " + ext);
+            } else {
+                LOGGER.debug("adding ModelConverter: " + ext);
+            }
+        }
+    }
+
     private static class outer1 {
         private void node1() {
             new MyClass().myMethod();
